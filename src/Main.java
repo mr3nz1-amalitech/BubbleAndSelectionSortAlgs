4c66a786bbe71c50f715173347c7b6d04ff12e0d
Murenzi Paterne <murenzi419@gmail.com>
2024-07-03 10:23:15 +0200

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     static int[] bubbleSort(int[] arr) {
         boolean swapped = false;
+
+//      loop through the array and swap unsorted consecutive numbers till the array is sorted. In an iteration completes without swapping any number, the loop will terminate
         while (!swapped) {
             boolean swappedValue = false;
             for (int i = 0; i < arr.length; i++) {
@@ -26,6 +26,7 @@ public class Main {
     }
 
     static int[] selectionSort(int[] arr) {
+//      We will maintain a sorted right side and unsorted left side. We will be in search of a value less that the current index from that index up to the maximum index. After we find it we will swap the values
         for (int i = 0; i < arr.length; i++) {
             for (int j = i; j < arr.length; j++) {
                 if (arr[j] < arr[i]) {
@@ -35,16 +36,17 @@ public class Main {
                 }
             }
         }
+
+
         return arr;
     }
 
     public static void main(String[] args) {
         String inputStr;
 
-
+//      Get input
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a series of numbers separated by spaces: ");
-
         inputStr = scanner.nextLine();
 
         String[] numsStr = inputStr.split(" ");
@@ -54,6 +56,7 @@ public class Main {
             nums[i] = Integer.parseInt(numsStr[i]);
         }
 
+//        Choose sorting algorithm
         System.out.println("Select sort algorithm: \n 1. Bubble sort \n 2. Selection sort ");
 
         if (scanner.nextInt() == 1) {
